6b472c1736b8c5fad72154e25121c34b113ebe80
Hanna-Jeanne <janghanna423@gmail.com>
2025-01-23 11:19:50 +0900

    [실습] React form input 컴포넌트

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -1,39 +1,57 @@
-import { useId } from 'react';
+import { useId, type ComponentProps } from 'react';
 
-interface FormInputProps {
-  type?:
-    | 'text'
-    | 'range'
-    | 'password'
-    | 'number'
-    | 'search'
-    | 'color'
-    | 'email';
+// interface FormInputProps {
+//   type?:
+//     | 'text'
+//     | 'range'
+//     | 'password'
+//     | 'number'
+//     | 'search'
+//     | 'color'
+//     | 'email';
+//   label: string;
+//   placeholder?: string;
+//   value?: string | number;
+//   defaultValue?: string | number;
+//   readOnly?: boolean;
+//   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+//   [property: string | number]: unknown;
+// }
+
+// interface FormInputProps extends ComponentProps<'input'> {
+//   label: string;
+// }
+// type FormInputProps = ComponentProps<'input'> & {
+//   label: string;
+// };
+
+// function FormInput({
+// type = 'text',
+// label,
+// placeholder,
+// value,
+// defaultValue,
+// readOnly,
+// onChange,
+// ...restProps
+// }: FormInputProps) {
+
+//   label,
+//   ...restProps
+// }: ComponentProps<'input'> & {
+//   label: string;
+// }) {
+
+type FormInputProps = ComponentProps<'input'> & {
   label: string;
-  placeholder?: string;
-  value?: string | number;
-  defaultValue?: string | number;
-  readOnly?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  [property: string | number]: unknown;
-}
-
-function FormInput({
-  type = 'text',
-  label,
-  placeholder,
-  value,
-  defaultValue,
-  readOnly,
-  onChange,
-  ...restProps
-}: FormInputProps) {
+};
+function FormInput({ label, ...restProps }: FormInputProps) {
   const inputId = useId();
 
   return (
     <div className="formControl">
       <label htmlFor={inputId}>{label}</label>
-      <input
+      {/* <input
         type={type}
         id={inputId}
         placeholder={placeholder}
@@ -42,9 +60,12 @@ function FormInput({
         value={value}
         onChange={onChange}
         {...restProps}
-      />
+      /> */}
+      <input id={inputId} {...restProps} />
     </div>
   );
 }
 
 export default FormInput;
+
+// export type FormInputProps = ComponentProps<typeof FormInput>;
--- src/playgrounds/02-reat-form.tsx
+++ src/playgrounds/02-reat-form.tsx
@@ -1,27 +1,102 @@
+import { useState } from 'react';
 import FormInput from '@/components/form-input';
+import FormRadio from '@/components/form-radio';
+
+const formStyles = {
+  display: 'flex',
+  flexFlow: 'column',
+  alignItems: 'start',
+  gap: 20,
+};
 
 function ReactForm() {
+  const [age, setAge] = useState<number>(22);
+  const [color, setColor] = useState<string>('#2483DB');
+  const [limitAge, setLimitAge] = useState<number>(40);
+
   return (
     <div className="ReactForm">
       <h2>React 폼(form)</h2>
-      <form>
-        <FormInput type="text" label="이름" placeholder="박수무당" />
+      <form style={formStyles}>
+        {/* type=text */}
+        <FormInput
+          // type="text"
+          label="이름"
+          placeholder="박수무당"
+        />
+
+        {/* type=password */}
         <FormInput
           type="password"
           label="비밀번호"
           placeholder="영어, 숫자 조합 4자리 이상"
         />
+
+        {/* type=email */}
+        <FormInput type="email" label="이메일" placeholder="user@company.io" />
+
+        {/* type=number, controlled component */}
         <FormInput
           type="number"
           label="나이"
-          defaultValue={20}
-          data-name="폼 인풋"
-          data-type="input"
-          title="컴포넌트 만세!"
-          v-for="data in list"
+          value={age}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target;
+            // const nextAgeValue: number = +value;
+            // const nextAgeValue: number = Number(value);
+            const nextAgeValue: number = parseInt(value, 10);
+            setAge(nextAgeValue);
+          }}
         />
-        <FormInput type="color" label="색상" />
-        <FormInput type="range" label="시청 허용 나이" />
+
+        {/* type=color */}
+        <FormInput
+          type="color"
+          label="색상"
+          value={color}
+          onChange={(e) => {
+            const { value } = e.target;
+            setColor(value);
+          }}
+        />
+
+        {/* type=range */}
+        <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
+          <FormInput
+            type="range"
+            label="시청 허용 나이"
+            min={10}
+            max={90}
+            step={10}
+            value={limitAge}
+            onChange={(e) => {
+              const { value } = e.target;
+              setLimitAge(parseInt(value, 10));
+            }}
+          />
+          <output>{limitAge}</output>
+        </div>
+
+        {/* type=file */}
+        <FormInput label="프로필" type="file" accept="image/*" multiple />
+
+        {/* type=radio */}
+        <fieldset>
+          <legend>성별</legend>
+          <FormRadio
+            label="남성"
+            value="남성"
+            name="usergender"
+            defaultChecked
+          />
+          <FormRadio label="여성" value="여성" name="usergender" />
+        </fieldset>
+
+        {/* type=checkbox */}
+
+        {/* type=date */}
+
+        {/* type=datetime-local */}
 
         <button type="submit">제출</button>
       </form>
