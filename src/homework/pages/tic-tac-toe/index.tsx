c446670a1ae0d11cc98224597630e33bb2a6d50f
Hanna-Jeanne <janghanna423@gmail.com>
2025-02-06 10:25:13 +0900

    [실습] Cell 버튼 상호작용 구현 - 포괄적인 비활성 버튼 설정 - disabled vs. aria-disabled

--- src/homework/pages/tic-tac-toe/index.tsx
+++ src/homework/pages/tic-tac-toe/index.tsx
@@ -4,10 +4,7 @@ import History from './components/history';
 
 function TicTacToe() {
   return (
-    <article className={tm(
-      'flex space-x-5 justify-center', 
-      'mt-10'
-    )}>
+    <article className={tm('flex space-x-5 justify-center', 'mt-10')}>
       <h2 className="sr-only">틱택토 게임</h2>
       <Board />
       <History />
